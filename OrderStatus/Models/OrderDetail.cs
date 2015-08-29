9aabfcdbe4fad8f0b2d39b9e6bf24c7177ad5675
Ivan Sued <thefenry@gmail.com>
2015-08-29 16:48:55 -0700

    Created Order Model

---
+++ OrderStatus/Models/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderStatus.Models
+{
+    class OrderDetail
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string ClientName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public byte Image { get; set; }
+        public virtual ApplicationUser CreatedBy { get; set; }
+    }
+}
