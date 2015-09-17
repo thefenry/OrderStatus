7ab760eed3876723fa8a94451b0bc8144898e322
Ivan Sued <thefenry@gmail.com>
2015-09-16 20:59:39 -0700

    Reverted to previous Changes and removed project for deployement.
    Was having issues with something changing in the solution while
    publishing API. It is fixed for now but I need to check solution in a
    different Desktop.

--- OrderStatus/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace OrderStatus.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
---
+++ OrderStatusAPI/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OrderStatusAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
