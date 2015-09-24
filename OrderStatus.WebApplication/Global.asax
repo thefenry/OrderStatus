0edd937f876ace38a7d1e6b19b93d78ca750b874
Ivan Sued <thefenry@gmail.com>
2015-09-23 18:57:18 -0700

    Create New Web App

---
+++ OrderStatus.WebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OrderStatus.WebApplication.MvcApplication" Language="C#" %>
---
+++ OrderStatus.WebApplication/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderStatus.WebApplication.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+    }
+}
