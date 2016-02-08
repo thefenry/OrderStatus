cfbb02c74849f95249cf0a4990a635cc836e92eb
Ivan Sued <thefenry@gmail.com>
2016-02-07 18:32:10 -0800

    Completed a modal to show data based on click.

--- OrderStatus.WebApplication/Scripts/Index/OrderController.js
+++ OrderStatus.WebApplication/Scripts/Index/OrderController.js
@@ -86,12 +86,40 @@
          });
         $scope.$apply();
     };
+    
+    // Get the modal
+    var modal = document.getElementById('myModal');
 
-    $(".modal-link").on('click', function (e) {
-        e.preventDefault();
-        $(this).attr('data-target', '#OrderDetailContainer');
-        $(this).attr('data-toggle', 'modal');
-    });
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
 
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+    $scope.detail = {};
+
+    $scope.openModal = function (id) {
+
+        $scope.detail = searchItem(id);
+        modal.style.display = "block";
+
+    }
+
+    function searchItem(id) {
+        for (var i = 0; i < dataResponse.length; i++) {
+            if (dataResponse[i].ID === id) {
+                return dataResponse[i];
+                break;
+            }
+        }
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
 }]);
