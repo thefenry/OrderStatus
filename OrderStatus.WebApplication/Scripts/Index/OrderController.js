c8c406cfbd03b5a28374dc4cde318404c81f5bfd
Ivan Sued <thefenry@gmail.com>
2016-02-09 21:41:50 -0800

    Started styling the boxes.

--- OrderStatus.WebApplication/Scripts/Index/OrderController.js
+++ OrderStatus.WebApplication/Scripts/Index/OrderController.js
@@ -91,15 +91,15 @@
     var modal = document.getElementById('myModal');
 
     // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+    //var btn = document.getElementById("myBtn");
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on the button, open the modal 
-    btn.onclick = function () {
-        modal.style.display = "block";
-    }
+    //btn.onclick = function () {
+    //    modal.style.display = "block";
+    //}
     $scope.detail = {};
 
     $scope.openModal = function (id) {
